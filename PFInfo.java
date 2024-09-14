7e6ead7a1d8fa9cdedcdde0179dc3b6f02a56326
unknown <elamschwey@outlook.com>
2024-09-14 12:33:38 -0400

    probably screwed this up

---
+++ PFInfo.java
@@ -0,0 +1,15 @@
+public class PFInfo {
+    public static void main(String[] args) {
+        String firstname = "Danny";
+        String lastname = "Washed";
+        String address = "40 Mil Rd";
+        String city = "East Rutherford";
+        String zip = "07073";
+
+        System.out.println("firstname: " + firstname);
+        System.out.println("lastname: " + lastname);
+        System.out.println("address: " + address);
+        System.out.println("city: " + city);
+        System.out.println("zip: " + zip);
+    }
+}
